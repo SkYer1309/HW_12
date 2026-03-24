eaa57d8ec63c6abe96624febe6927ee6ea9d8fe2
admin <skyer13@mail.ru>
2026-03-24 08:57:45 +0300

    Revert "Закончено ДЗ"
    This reverts commit 3a3bd7c1cbbc446257285047c2356cf23b35dd82.

--- homework12/src/Main.java
+++ homework12/src/Main.java
@@ -2,19 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Author tolkien = new Author("Джон", "Толкиен");
-        System.out.println("Автор 1: " + tolkien);
-        Author perumov = new Author("Ник", "Перумов");
-        System.out.println("Автор 2: " + perumov);
-
-        Book bookOne = new Book("Властелин колец", tolkien, 1955);
-
-        Book bookTwo = new Book("Война мага", perumov, 2008);
-        System.out.println("Книга 1: " + bookOne);
-        System.out.println("Книга 2: " + bookTwo);
-
-        bookTwo.year = 2010;
-        System.out.println("Книга 2: " + bookTwo);
 
     }
 }
